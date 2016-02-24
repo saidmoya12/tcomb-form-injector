67c0a55b45e724206af3da6d6c7c5d7e36c6dc4f
Ekoo20 <smoya@ekoomedia.co>
2016-02-24 16:34:37 -0500

    *

--- README.md
+++ README.md
@@ -1,41 +1,40 @@
 ###Example complex component
 
-	import t from 'tcomb-form'		//@see: https://github.com/gcanti/tcomb-form
+  import t from 'tcomb-form'    //@see: https://github.com/gcanti/tcomb-form
 
-	//sample with datetime picker
-	import DateTime 	from 'react-datetime'; //@see: https://github.com/YouCanBookMe/react-datetime
+  //sample with datetime picker
+  import DateTime   from 'react-datetime'; //@see: https://github.com/YouCanBookMe/react-datetime
 
-	import TFormFactoryInjector		from from 'tcomb-form-injector'
+  import TFormFactoryInjector    from from 'tcomb-form-injector'
 
-	var formOptions = {
-		fields:{
-			date: {
-				factory: TFormFactoryInjector.InjectorFactory,
-				inject: {
-					attrs:		{placeholder: 'Reservación'}, //tcomb attrs
-					component:	DateTime,
-					valueProp: 'defaultValue',
-					props: {		//component props
-						className: 'customClassname'
-					},
-					event: 'onBlur', callback: function(locals, date){
-						//update value
-						locals.onChange(date.format('YYYY-MM-DD HH:mm:ss'));
-					}
-				}
-			}
-		}
-	}
+  var formOptions = {
+    fields:{
+      date: {
+        factory: TFormFactoryInjector.InjectorFactory,
+        inject: {
+          attrs:    {placeholder: 'Reservación'}, //tcomb attrs
+          component:  DateTime,
+          valueProp: 'defaultValue',
+          props: {    //component props
+            className: 'customClassname'
+          },
+          event: 'onBlur', callback: function(locals, date){
+            //update value
+            locals.onChange(date.format('YYYY-MM-DD HH:mm:ss'));
+          }
+        }
+      }
+    }
+  }
 
 
-#Default settings
-inject: {
-	attrs: 			null,		//tcomb-form attrs
-	component:		null,		//required component
-	valueProp:		'value',
-	props: 			{},			//component props
-	event:			'onChange',
-	callback:		function(locals, value){
-		locals.value = value
-	}
-}
+###Default settings
+
+  inject: {
+    valueProp:    'value',
+    props:       {},      //component props
+    event:      'onChange',  //component event type
+    callback:    function(locals, value){
+      locals.onChange(value);
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,153 @@
+/**
+*
+*/
 'use strict';
 
-import InjectorFactory 		from './InjectorFactory';
-import Factories			from './Factories';
-import type					from './Type';
+import React				from 'react';
+import classNames			from 'classnames';
+import _					from 'underscore';
+import t					from 'tcomb-form'
 
-exports.InjectorFactory = InjectorFactory;
-exports.factory = Factories;
+export default class InjectorFactory extends React.Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			isValid:	true,
+			Element:	this.createElement(props.options, props.value),
+			value:		this.setValue(props.value, true),
+			elementValue: props.value
+		};
+	}
+
+	//update component
+	componentWillReceiveProps(nextProps){
+		this.setState({
+			Element: 		this.createElement(nextProps.options, nextProps.value),
+			elementValue:	nextProps.value,
+			value:			this.setValue(nextProps.value, true)
+		});
+	}
+
+	onChange(value, path){
+		this.setState({
+			value: value
+		}, function(){
+			this.props.onChange(value, this.props.ctx.path);
+		})
+	}
+
+	setValue(value, returnData){
+		let elementValue = value;
+
+		if(_.isObject(value)){
+			if(value.id !== undefined){
+				value = value.id;
+			}else if(value.key !== undefined){
+				value = value.key;
+			}
+		}
+
+		if(returnData == true) return value;
+	}
+
+	validate(){
+		let result = t.validate(this.state.value, this.props.type, this.props.ctx.path);
+		this.setState({
+			isValid: result.isValid()
+		})
+		return result;
+	}
+
+	getDefaultLabel(){
+
+	}
+
+	getLabel() {
+		var opts = this.props.options || {};
+	    var ctx = this.props.ctx;
+
+		// handling label
+	    var label = opts.label;
+	    if (!label && ctx.auto === 'labels') {
+	      // if labels are auto generated, get the default label
+	      label = this.getDefaultLabel();
+	    }
+
+	    if(label === undefined){
+	    	return null;
+	    }
+
+	    return (<label id={opts.id}>{label}</label>);
+	}
+
+	getPlaceHolder(){
+		var opts = this.props.options || {};
+	    var ctx = this.props.ctx;
+
+		var placeholder = null;
+	    // labels have higher priority
+	    if (ctx.auto !== 'none') {
+	    	placeholder = !t.Nil.is(opts.placeholder) ? opts.placeholder : ctx.label;
+	    }
+
+	    return placeholder;
+	}
+
+
+	createElement(options, value){
+		let {component, props, event, callback, valueProp} = options.inject;
+
+		props.placeholder = this.getPlaceHolder();
+		if(options.attrs !== undefined){
+			props = _.extend(options.attrs, props);
+		}
+
+		props.key = Math.floor((Math.random() * 100) + 1);
+
+		valueProp = valueProp || 'value';
+		props[valueProp] = value;
+
+		event = event || 'onChange';
+		if(callback !== undefined){
+			props[event] = callback.bind(null, this);
+		}else{
+			props[event] = function(evt){
+				this.onChange(evt.target.value);
+			}
+		}
+
+		return React.createElement(component, props);
+	}
+
+	render(){
+		let {Element} = this.state;
+
+		let label = this.getLabel();
+
+		let classes = classNames({
+			field: true,
+			error: !this.state.isValid
+		});
+
+		return <div className={classes}>
+			{label}{Element}
+		</div>;
+	}
+}
+
+InjectorFactory.defaultProps = {
+	value: null
+}
+
+InjectorFactory.propTypes = {
+	value: React.PropTypes.any,
+	inject: React.PropTypes.shape({
+		component:	React.PropTypes.object.isRequired,
+		props:		React.PropTypes.object,
+		event:		React.PropTypes.string,
+		callback:	React.PropTypes.func,
+		valueProp:	React.PropTypes.string
+	})
+}
