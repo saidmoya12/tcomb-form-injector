41e7aaa91bd057a136809d4e751072c029973262
Ekoo20 <smoya@ekoomedia.co>
2016-02-24 10:05:44 -0500

    Fix version, test1 import

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
+/**
+* @author: Said Moya Wilches
+*/
 
 import InjectorFactory 		from './InjectorFactory';
 import Factories			from './Factories';
 import type					from './Type';
 
-exports.InjectorFactory = InjectorFactory; 
+exports.InjectorFactory = InjectorFactory;
 exports.factory = Factories;
